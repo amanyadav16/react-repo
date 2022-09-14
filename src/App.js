18ee6a4de9f580e32ee7bcbcf068109c8b8bd7b6
amanyadav16 <amanyadav16011998@gmail.com>
2022-09-15 00:20:38 +0530

    push test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   let b1='box1';
   let b2='box2';
   let b3='box3';
+  let b4='box4';
+  let b5='box5';
+  let b6='box6';
 
   return (
    <>
@@ -12,9 +15,9 @@ function App() {
     <Box boxName={b1} />
     <Box boxName={b2} />
     <Box boxName={b3} />
-    <Box boxName={b3} />
-    <Box boxName={b3} />
-    <Box boxName={b1} />
+    <Box boxName={b4} />
+    <Box boxName={b5} />
+    <Box boxName={b6} />
     </div>
     <hr></hr>
    </>
